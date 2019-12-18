c38ae0ce9915a3ec2cace069072a3a074874cb84
davidkazad <14ka135@gmail.com>
2019-12-18 22:33:12 +0200

    creation de la base applicative  * home page  * login page  * payment method page  * sign up activity  * maps

---
+++ app/src/main/java/com/maishapay/mjeks/CompletePaymentActivity.java
@@ -0,0 +1,14 @@
+package com.maishapay.mjeks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletePaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_payment);
+    }
+}
